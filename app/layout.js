b272a75e887b4ff20f2ee1d84be7b84bee30d182
Noel Paing Oak Soe <u6530183@au.edu>
2024-12-12 23:28:18 +0700

    import err

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,17 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Stock App",
-  description: "Stock App",
-};
+  title: 'Stock App',
+  description: 'Stock App',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <ResponsiveAppBar />
-        {children}
-        </body>
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
