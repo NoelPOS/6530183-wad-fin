d23f405bd10edbd002961a60a160ae87734b4290
Noel Paing Oak Soe <u6530183@au.edu>
2024-12-16 14:11:28 +0700

    change meta data

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Stock App',
-  description: 'Stock App',
+  title: 'Hive Dashboard',
+  description: 'Admin Dashboard for Hive',
 }
 
 export default function RootLayout({ children }) {
