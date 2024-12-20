1b3eeb052a4a1fe0d6856b69fd0c5c4249144b8c
NoelPOS <u6530183@au.edu>
2024-12-20 09:56:16 +0700

    localstorage fix2

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,13 @@ import { LoginForm } from '@/components/auth/login-form'
 import { AdminPanel } from '@/components/admin-panel'
 import { useAuthStore } from '@/zustand/admin-store'
 import { useEffect, useState } from 'react'
+import { set } from 'mongoose'
 
 export default function AdminPage() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   useEffect(() => {
     if ( typeof window !== 'undefined'){
-      isAuthenticated = localStorage.getItem('isAuthenticated')
+      setIsAuthenticated(localStorage.getItem('isAuthenticated'))
     }
   })
   return isAuthenticated ? <AdminPanel /> : <LoginForm />
